3a4769d883af4fbe5266ed642c632a295fb12b80
adramelech <adramelech@44a22ff1-f3e3-9744-8249-f91a3457a9a2>
2010-02-21 20:47:23 +0000

    anub'arak (toc) spell ids and typo fix
    git-svn-id: svn://www.phoenix-wow.com/PhoenixEmu@1746 44a22ff1-f3e3-9744-8249-f91a3457a9a2

--- scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_anubarak_toc.cpp
+++ scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_anubarak_toc.cpp
@@ -3,6 +3,28 @@
 
 //evidently, azjol-nerub was merely a set-back
 
+enum Spells
+{
+    SPELL_BERSERK               = 26662,
+    SPELL_FREEZING_SLASH        = 66012,
+    SPELL_PENETRATING_COLD_N10  = 66013,
+    SPELL_PENETRATING_COLD_N25  = 67700,
+    SPELL_PENETRATING_COLD_H10  = 68509,
+    SPELL_PENETRATING_COLD_H25  = 68510,
+    SPELL_LEECHING_SWARM_N10    = 66118,
+    SPELL_LEECHING_SWARM_N25    = 67630,
+    SPELL_LEECHING_SWARM_H10    = 68646,
+    SPELL_LEECHING_SWARM_H25    = 68647,
+    SPELL_PURSUING_SPIKES       = 65922,  // only part of the effect, probably used by dummy creature
+};
+
+enum AddIds
+{
+    NPC_FROST_SPHERE            = 34606,
+    NPC_NERUBIAN_BURROWER       = 34607,
+    NPC_SWARM_SCARAB            = 34605,
+};
+
 struct MANGOS_DLL_DECL boss_toc_anubarakAI: public boss_trial_of_the_crusaderAI
 {
     boss_toc_anubarakAI(Creature* pCreature):
