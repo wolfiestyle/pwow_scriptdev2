43ddf6560442e3a5dd0fb214df5262f93b556199
TheMonkey1000 <TheMonkey1000@44a22ff1-f3e3-9744-8249-f91a3457a9a2>
2010-10-23 03:42:42 +0000

    - Fix paladin quest 'Redeeming the Dead'
    git-svn-id: svn://www.phoenix-wow.com/PhoenixEmu@2416 44a22ff1-f3e3-9744-8249-f91a3457a9a2

--- scripts/eastern_kingdoms/silvermoon_city.cpp
+++ scripts/eastern_kingdoms/silvermoon_city.cpp
@@ -70,7 +70,7 @@ struct MANGOS_DLL_DECL npc_blood_knight_stillbladeAI : public ScriptedAI
         if ((Spellkind->Id == SPELL_SHIMMERING_VESSEL) && !spellHit &&
             (Hitter->GetTypeId() == TYPEID_PLAYER) && (((Player*)Hitter)->IsActiveQuest(QUEST_REDEEMING_THE_DEAD)))
         {
-            ((Player*)Hitter)->AreaExploredOrEventHappens(QUEST_REDEEMING_THE_DEAD);
+            ((Player*)Hitter)->KilledMonsterCredit(m_creature->GetEntry());
             DoCastSpellIfCan(m_creature,SPELL_REVIVE_SELF);
             m_creature->SetStandState(UNIT_STAND_STATE_STAND);
             m_creature->SetUInt32Value(UNIT_DYNAMIC_FLAGS, 0);
